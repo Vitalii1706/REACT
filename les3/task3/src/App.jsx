de81cd05a358e82b80f09fe4318bfc3b7b7f23be
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-14 19:35:28 +0200

    1

---
+++ les3/task3/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Comment from './Comment';
+
+const userInfo = {
+    name: 'Tom',
+    author: 'Anonimus',
+    avatarUrl: 'https://avatars1.githubusercontent.com/u10002'
+}
+
+
+const App = () => {
+ return (
+    <Comment
+    user={userInfo}
+    text="Good job!!!"
+    date={new Date('2022-01-14')}
+    />
+  )
+};
+
+export default App;
--- les3/task3/src/index.jsx
+++ les3/task3/src/index.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './comments.scss';
 import './styles.scss';
-import Comment from './Comment.jsx';
+import App from './App.jsx';
 
 
 const rootElement = document.querySelector('#root');
 
-const userInfo = {
-    name: 'Tom',
-    author: 'Anonimus',
-    avatarUrl: 'https://avatars1.githubusercontent.com/u10002'
-}
+
 
 ReactDOM.render(
-    < Comment
-    user={userInfo}
-    text="Good job!!!"
-    date={new Date('2022-01-14')}
-     />, 
+    < App />, 
      rootElement
      );
