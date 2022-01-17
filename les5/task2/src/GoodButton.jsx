2927c58c67cf075694522750b353a9fd7926c2b4
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-17 16:27:34 +0200

    3

--- les5/task2/src/GoodButton.jsx
+++ les5/task2/src/GoodButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class GoodButton extends React.Component {
     handleClick(e) {
-        alert('Good job!');
+        alert(e.target.textContent);
     }
 
     render() {
