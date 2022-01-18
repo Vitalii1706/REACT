de5b6ddcd9a8164d4e59a04fcd4590efe4cc0aac
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-18 15:26:03 +0200

    1

---
+++ les6/task1/src/Auth.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting.jsx';
+import Login from './Login.jsx';
+import Logout from './Logout.jsx';
+
+class Auth extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false,
+        };
+    }
+
+    handleLogin = () => {
+        this.setState({ isLoggedIn: true, });
+    };
+
+    handleLogout = () => {
+        this.setState({ isLoggedIn: false, });
+    };
+
+    render() {
+    let button;
+    
+        if (this.state.isLoggedIn) {
+            button = <Logout onLogout={this.handleLogout} />;
+        } else {
+            button = <Login onLogin={this.handleLogin} />;
+        }
+        return (
+            <div className='panel'>
+                <Greeting isLoggedIn={this.state.isLoggedIn}/>
+                <div>{button}</div>
+            </div>
+        );
+    }
+}
+
+export default Auth;
---
+++ les6/task1/src/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logout = (props) => {
+    return (
+        <button className="btn logout" onClick={props.onLogout}>Logout</button>
+    );
+};
+
+export default Logout;
