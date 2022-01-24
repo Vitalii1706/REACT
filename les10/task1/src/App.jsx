29ba9093f5edb7a106943cde867c54830789381f
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-24 14:19:28 +0200

    5

--- les10/task1/src/App.jsx
+++ les10/task1/src/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   fetchUserData = userId => {
-    const userUrl = `https:/api.github.com/users/${userId}`;
+    const userUrl = `https://api.github.com/users/${userId}`;
     fetch(userUrl)
       .then(response => response.json())
       .then(userData =>
--- les10/task1/src/UserProfile.jsx
+++ les10/task1/src/UserProfile.jsx
@@ -5,21 +5,18 @@ class UserProfile extends Component {
     if (!this.props.userData) {
       return null;
     }
-    const { name, location, avatar_url } = this.props.userData;
-
+    const { avatar_url, location, name } = this.props.userData;
     return (
       <div className="user">
-        <img
-          alt="User Avatar"
-          src={avatar_url} // "https://avatars3.githubusercontent.com/u/69631?v=4"
-          className="user__avatar"
-        />
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
         <div className="user__info">
           <span className="user__name">{name}</span>
+
           <span className="user__location">{location}</span>
         </div>
       </div>
     );
   }
 }
+
 export default UserProfile;
