fad1b1442efa5e3b28389087af2234a240742d05
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-25 18:58:53 +0200

    1

---
+++ react task onlayn/src/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './login.jsx';
+import Logout from './Logout.jsx';
+import Spinner from './Spinner.jsx';
+
+const Auth = () => (
+  <>
+    <Login />
+    <Logout />
+    <Spinner />
+  </>
+);
+
+export default Auth;
---
+++ react task onlayn/src/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// in: func
+// out: JSX
+
+const Login = () => <button className="login btn">Login</button>;
+
+export default Login;
