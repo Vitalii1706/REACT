f58f2c37010bf164c3a3f2b570fe3db625760206
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-29 13:54:08 +0200

    1

---
+++ les12/task1/src/CreateTaskInput.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class CreateTaskInput extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleTaskCreate = () => {
+    this.props.onCreate(this.state.value);
+    this.setState({
+      value: '',
+    });
+  };
+
+  render() {
+    return (
+      <div className="create-task">
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          className="create-task__input"
+        />
+        <button className="btn create-task__btn" onClick={this.handleTaskCreate}>
+          Create
+        </button>
+      </div>
+    );
+  }
+}
+export default CreateTaskInput;
+/*
+1. текст з інпута
+2. зробити таску з цього тексту
+3. добавити новий елемент до списку
+*/
