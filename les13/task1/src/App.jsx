5f65f2a22f3f76a32397149a8bb1b2a20f8d9181
Vitalii1706 <kitral.v.o@gmail.com>
2022-01-29 21:11:46 +0200

    2

--- les13/task1/src/App.jsx
+++ les13/task1/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home.jsx';
 import Products from './Products.jsx';
 import Contacts from './Contacts.jsx';
@@ -8,12 +8,20 @@ import PageNotFound from './PageNotFound.jsx';
 const App = () => (
   <div className="page">
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
     </BrowserRouter>
   </div>
 );
